7eb961dd477add55447b0689ea26084c676edbe4
Sebastian Garcia <zebasxx@hotmail.com>
2019-04-16 07:37:41 -0700

    Adding 2nd Template

---
+++ _Template_DifferencingVHD.ps1
@@ -0,0 +1,21 @@
+<#
+Description of elemnts in the multidimentional array
+("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "VHD Parent Path", "VHD Path\VHD Mame")
+#>
+Clear-Host
+
+$VMs =  @("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "VHD Parent Path", "VHD Path\VHD Mame"), 
+        @("Second VM"), 
+        @("Third VM and so on")
+
+
+#New-VHD -ParentPath "C:\Base Images\WinSrv2016C- LTS - Base.vhdx" -Path "D:\VMs\VHDs\SwarmLab\SLWNode01.vhdx" -Differencing;New-VM -Name SLWNod01 -MemoryStartupBytes 4096mb -BootDevice VHD -VHDPath "D:\VMs\VHDs\SwarmLab\SLWNode01.vhdx" -Generation 1 -Switch "192.168.0.0/24";Set-VMProcessor SLWNod01 -Count 2 -ExposeVirtualizationExtensions $true
+
+foreach ($VM in $VMs){
+
+    
+
+}
+
+Start-Sleep -Seconds 5
+
--- _Template_NewVHD.ps1
+++ _Template_NewVHD.ps1
@@ -1,10 +1,10 @@
 <#
 Description of elemnts in the multidimentional array
-("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "VHD Parent Path", "VHD Path\VHD Mame")
+("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "ISO Path", "VHD Path\VHD Mame", "VHD Size")
 #>
 Clear-Host
 
-$VMs =  @("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "VHD Parent Path", "VHD Path\VHD Mame"), 
+$VMs =  @("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtualiz, $true or $false", "ISO Path", "VHD Path\VHD Mame", "VHD Size"), 
         @("Second VM"), 
         @("Third VM and so on")
 
@@ -12,9 +12,11 @@ $VMs =  @("VM Name", "'Memoy'mb", "Viryual Switch Name", "VCPU", "Nested Virtual
 #New-VHD -ParentPath "C:\Base Images\WinSrv2016C- LTS - Base.vhdx" -Path "D:\VMs\VHDs\SwarmLab\SLWNode01.vhdx" -Differencing;New-VM -Name SLWNod01 -MemoryStartupBytes 4096mb -BootDevice VHD -VHDPath "D:\VMs\VHDs\SwarmLab\SLWNode01.vhdx" -Generation 1 -Switch "192.168.0.0/24";Set-VMProcessor SLWNod01 -Count 2 -ExposeVirtualizationExtensions $true
 
 foreach ($VM in $VMs){
-
-    Write-Output $VM
-
+    Write-Host "-------------"
+    foreach ($Object in $VM){
+        Write-Host $Object" - " -NoNewline
+    }
+    Write-Host ""
 }
 
 Start-Sleep -Seconds 5
